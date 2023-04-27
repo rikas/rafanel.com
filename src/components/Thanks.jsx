b617351651d95e2c8f3e8a2e9a928b8f4439a152
Ricardo Otero <oterosantos@gmail.com>
2023-04-27 16:25:48 +0100

    Change the header in thanks page

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -5,7 +5,8 @@ import Title from './Title';
 const Thanks = ({ onClose }) => (
   <div>
     <div className="p-5">
-      <Title variant="h3">Obrigado pela vossa presença! 🎉</Title>
+      <Title variant="h3">Obrigada/o</Title>
+      <Title variant="h3">Até breve! 🎉</Title>
     </div>
 
     <div className="flex bg-gray-100 px-4 py-5 sm:px-6 sm:py-8 mt-5 justify-end">
